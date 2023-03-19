4a0e79d6ce850d419b7aae78a370b55da3c89c3a
rusirugunaratne <info.rusiru@gmail.com>
2023-03-19 22:01:21 +0530

    learn added

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import program
 
 
 if __name__ == '__main__':
     # train.train_model("data/df3.csv","data/holidays-2.csv")
-    program.train_console()
-    program.predict_console()
+    print("Hi")
